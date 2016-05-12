9f13ed8f42caf65a4140a01727d66b7d67871149
Vesa Meskanen <vesa.meskanen@logica.com>
2016-05-12 10:27:48 +0300

    New test data (old OpenAddr city data sets) and a test generator script for them

---
+++ scripts/importAddress.js
@@ -0,0 +1,62 @@
+var csv_parse = require('csv-parse');
+var fs = require('fs');
+var through = require('through2');
+
+var importTestCases = require('./testCaseImporter');
+var csv_parser = csv_parse({ delimiter: ',', columns: true});
+var filename = process.argv[2];
+var read_stream = fs.createReadStream(filename);
+
+// import only % of test addresses.
+// set key value to variate the test: for example 'node importAddr.js helsinki.csv 31'
+var key = process.argv[3] || 1;
+
+var test_file_json = {
+  name: 'HSL address tests',
+  description: 'Address list from an old OpenAddresses entry for Helsinki region',
+  priorityThresh: 4,
+  distanceThresh: 300, // meters
+  normalizers: {
+    name: [ 'toUpperCase']
+  },
+};
+
+function getRandomInt() {
+  return Math.floor(Math.random() * 100);
+}
+
+var count = 0;
+
+var testCaseStream = through({objectMode: true}, function(record, encoding, callback) {
+  var test = {
+    id: count,
+    status: 'pass',
+    user: 'hsldevcom',
+    in: {
+      text: record.STREET + ' ' + record.NUMBER + ', ' + record.CITY
+    },
+    expected: {
+      properties: [
+        {
+          // enable name property for strict name comparison
+          name: record.STREET + ' ' + record.NUMBER,
+          locality: record.CITY
+        }
+      ],
+      coordinates: [
+        record.LON, record.LAT
+      ]
+    }
+  };
+  if(key === getRandomInt()) {
+    count++;
+    this.push(test);
+  }
+  callback();
+});
+
+var full_stream = read_stream.pipe(csv_parser).pipe(testCaseStream);
+
+var prefix = process.argv[2].split('.')[0] + 'AddressTest'; // output name from param file name
+
+importTestCases(prefix, test_file_json, full_stream, 200000);
