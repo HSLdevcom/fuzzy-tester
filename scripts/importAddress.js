3a14c8218be06a78f99882514d06511691c40956
Vesa Meskanen <vesa.meskanen@logica.com>
2016-06-03 09:34:02 +0300

    Fix output path to use basename without path. Convert key param to int.

--- scripts/importAddress.js
+++ scripts/importAddress.js
@@ -1,7 +1,7 @@
 var csv_parse = require('csv-parse');
 var fs = require('fs');
 var through = require('through2');
-
+var path = require('path');
 var importTestCases = require('./testCaseImporter');
 var csv_parser = csv_parse({ delimiter: ',', columns: true});
 var filename = process.argv[2];
@@ -10,6 +10,7 @@ var read_stream = fs.createReadStream(filename);
 // import only % of test addresses.
 // set key value to variate the test: for example 'node importAddr.js helsinki.csv 31'
 var key = process.argv[3] || 1;
+key = parseInt(key);
 
 var test_file_json = {
   name: 'HSL address tests',
@@ -58,6 +59,6 @@ var testCaseStream = through({objectMode: true}, function(record, encoding, call
 
 var full_stream = read_stream.pipe(csv_parser).pipe(testCaseStream);
 
-var prefix = process.argv[2].split('.')[0] + 'AddressTest'; // output name from param file name
+var prefix = path.basename(filename).split('.')[0] + 'AddressTest'; // output name from param file name
 
 importTestCases(prefix, test_file_json, full_stream, 200000);
